44b549fc03813637d809ed874c40e02084385b4c
Gabe Pesco <gabepesco@gmail.com>
2021-04-25 20:08:21 -0400

    made the iteration apply sparsity changes in batches for a mild speedup

--- iterate_model.py
+++ iterate_model.py
@@ -32,20 +32,40 @@ def main():
 		current_pref = get_next_pref(current_pref, model)
 
 
-def get_next_pref(pref, model, batch_size=1000):
+def get_next_pref(pref, model, batch_size=50):
 	m, n = np.shape(pref)
 	new_pref = sp.lil_matrix((m, n))
 	lengths = functions.get_playlist_length_samples(n=m)
-	for i in tqdm(range(m)):
-		recs = model.recommend(userid=i,
-								user_items=pref,
-								N=lengths[i],
-								filter_items=list(pref[i, :].nonzero()[1]),
-								filter_already_liked_items=False,
-								recalculate_user=False)
-		indices, scores = zip(*recs)
-		new_pref[i, indices] = 1
+	num_batches = int(m / batch_size)
+
+	for b in tqdm(range(num_batches)):
+		vec = np.cumsum(lengths[b*batch_size: (b+1)*batch_size])
+		x = np.zeros((vec[-1],))
+		y = np.zeros((vec[-1],))
+		for i in range(batch_size):
+			idx = b*batch_size + i
+			recs = model.recommend(userid=idx,
+									user_items=pref,
+									N=lengths[idx],
+									filter_items=list(pref[idx, :].nonzero()[1]),
+									filter_already_liked_items=False,
+									recalculate_user=False)
+			indices, scores = zip(*recs)
+			if i == 0:
+				start = 0
+			else:
+				start = vec[i-1]
+			end = vec[i]
+			x[start:end] = idx
+			y[start:end] = indices
+		new_pref[(x, y)] = 1
 	return new_pref
 
 
 main()
+
+# y = np.zeros((10, ))
+# b = np.array([1, 2, 3])
+# print(y, b)
+# y[2:5] = b
+# print(y)
